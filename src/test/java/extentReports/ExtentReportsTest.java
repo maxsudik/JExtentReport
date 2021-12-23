b48be480f864549ba3bf2b6c23dbbdf615472f36
maxsudik <maxsudik@gmail.com>
2021-12-23 10:14:17 -0800

    Adding second test and changed location of the generated report

--- src/test/java/extentReports/ExtentReportsTest.java
+++ src/test/java/extentReports/ExtentReportsTest.java
@@ -11,20 +11,28 @@ public class ExtentReportsTest {
     @Test
     public void extentTest() {
         ExtentReports extent = new ExtentReports();
-        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        ExtentSparkReporter spark = new ExtentSparkReporter("src/test/java/reports/index.html");
         spark.config().setTheme(Theme.DARK);
         spark.config().setDocumentTitle("My Report");
         spark.config().setReportName("Max Sudik");
         extent.attachReporter(spark);
 
-        ExtentTest test = extent.createTest("Login Test");
-        test.pass("Login Test started successfully");
-        test.pass("URL is loaded");
-        test.pass("Value entered");
-        test.pass("Login Test completed successfully");
-        test.fail("Test Failed here");
-        test.info("This is some info for testing logs");
+        ExtentTest test1 = extent.createTest("Login Test");
+        test1.pass("Login Test started successfully");
+        test1.pass("URL is loaded");
+        test1.pass("Value entered");
+        test1.pass("Login Test completed successfully");
+        test1.fail("Test Failed here");
+        test1.info("This is some info for testing logs");
 
+        ExtentTest test2 = extent.createTest("Home Page test");
+        test2.pass("Login Test started successfully");
+        test2.pass("URL is loaded");
+        test2.pass("Value entered");
+        test2.pass("Login Test completed successfully");
+        test2.fail("Test Failed here");
+        test2.info("This is some info for testing logs");
 
+        extent.flush();
     }
 }
